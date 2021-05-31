a5442f249eaebca7625190f8760be17eafb4792a
user <user@reza>
2021-05-31 09:15:03 +0700

    https://github.com/reza0107/modul-5.git

---
+++ src/Slinkedlist/SinglyLinkedListApp.java
@@ -0,0 +1,23 @@
+package Slinkedlist;
+public class SinglyLinkedListApp {
+    public static void main(String[] ar)
+    {
+        SinglyLinkedList lk = new SinglyLinkedList();
+        lk.insertAwal(10);
+        lk.display();
+        lk.insertAwal(20);
+        lk.display();
+        lk.insertAwal(30);
+        lk.display();
+        lk.insertAwal(40);
+        lk.display();
+        lk.insertAtPos(50, 2);
+        lk.display();
+        lk.insertAwal(60);
+        lk.display();
+        lk.insertAwal(2);
+        lk.display();
+    
+    }
+    
+}
